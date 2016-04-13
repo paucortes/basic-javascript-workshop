8c5eae9f21cfc248b447f10deedbfe4919481939
Pau <paucortes2@gmail.com>
2016-04-13 17:47:14 +0000

    Completed the second exercise

--- answers.js
+++ answers.js
@@ -7,3 +7,14 @@ function firstCharacter(anyString) {
     return anyString.substring(0,1);
 }
 console.log(firstCharacter("My name is Pau"));
+
+// This function takes a string and returns the last letter using .substring
+
+function lastCharacter(anyString) {
+    return anyString.substring(anyString.length -1,anyString.length);
+}
+console.log(lastCharacter("My name is Pau"));
+console.log(lastCharacter("Whatever"));
+console.log(lastCharacter("33"));
+console.log(lastCharacter(""));
+
