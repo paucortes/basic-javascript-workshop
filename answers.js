e22a7c8c407089f7209eb214eface62cefcf8fd0
Pau <paucortes2@gmail.com>
2016-04-13 17:34:48 +0000

    Completed the first exercise

--- answers.js
+++ answers.js
@@ -1,2 +1,9 @@
 // This is the first comment
 
+
+// This function takes a string and returns the first letter using .substring
+
+function firstCharacter(anyString) {
+    return anyString.substring(0,1);
+}
+console.log(firstCharacter("My name is Pau"));
